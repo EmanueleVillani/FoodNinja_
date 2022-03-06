680a4cfd98531e4ec091c87091cfaa643c03a55b
Emanuele Villani <manu.lebon@hotmail.it>
2022-03-06 19:11:56 +0100

    button animation
    added GameOvere animation text, button restart game

--- Assets/Scrips/GameManager.cs
+++ Assets/Scrips/GameManager.cs
@@ -2,40 +2,66 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     private int score;
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI textGameOver;
     public float spawnRate = 1.5f;
     public List<GameObject> targets;
+    public bool isGameActive ;
+    public GameObject restarButtona;
+
     void Start()
     {
+        isGameActive = true;
         score = 0;
         UpDateScore(0);
         StartCoroutine(SpawnTarget());
+       
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+
     }
 
     IEnumerator SpawnTarget()
     {
-        while (true)
+
+
+        while (isGameActive)
         {
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
-           
+
         }
-        
+
+
+
     }
     public void UpDateScore(int scoreToAdd)
     {
         score += scoreToAdd;
         scoreText.text = "Score " + score;
+    }
+
+    public void GameOver()
+    {
+        restarButtona.gameObject.SetActive(true);
+        textGameOver.gameObject.SetActive(true);
+        
+        isGameActive = false;
+    }
+
+    public void RestartButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
--- Assets/Scrips/Target.cs
+++ Assets/Scrips/Target.cs
@@ -13,16 +13,18 @@ public class Target : MonoBehaviour
     private float ySpawnPos = -2;
     private float maxTorque = 10;
     public int pointValue;
+
     void Start()
     {
         gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+
         targetRb = GetComponent<Rigidbody>();
         targetRb.AddForce(RandomForce(), ForceMode.Impulse);//far saltare gli oggetti in alto
         targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);// creare doppi oggetti spawnare
         transform.position = RandomSpawnPos(); // la possizione dove spawnare gli oggetti
     }
 
-   
+
     void Update()
     {
 
@@ -30,15 +32,26 @@ public class Target : MonoBehaviour
 
     private void OnMouseEnter()
     {
+        if (gm.isGameActive)
+        {
+            Instantiate(explosion, transform.position, explosion.transform.rotation);
+            Destroy(gameObject);
 
-        Instantiate(explosion, transform.position, explosion.transform.rotation);
-        Destroy(gameObject);
-        
-        gm.UpDateScore(pointValue);
+            gm.UpDateScore(pointValue);
+        }
+
     }
     public void OnTriggerEnter(Collider other)
     {
+
         Destroy(gameObject);
+
+        if (!gameObject.CompareTag("Bad"))
+        {
+            gm.GameOver();
+        }
+
+
     }
 
     Vector3 RandomForce()
